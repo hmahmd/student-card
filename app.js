3192059af901beb7586ade7ef3bdf9f81c32d1d8
Hafiz Muhammad Ahmed <hafizmuhammad.ahmed2025@gmail.com>
2025-11-24 12:06:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+function Submit(e) {
+  e.preventDefault();
+
+  let nameInput = document.getElementById("nameInput");
+  let rollNoInput = document.getElementById("rollNoInput");
+  let batchNoInput = document.getElementById("batchNoInput");
+  let showResult = document.getElementById("showResult");
+
+  let name = nameInput.value;
+  let rollNo = rollNoInput.value;
+  let batchNo = batchNoInput.value;
+
+  if (name === "" || rollNo === "" || batchNo === "") {
+    showResult.innerHTML = `<div class="result">Plz Fill All input</div>`;
+    return;
+  }
+
+  let data = {
+    name: name,
+    rollNumber: rollNo,
+    batch: batchNo,
+    id: Date.now(),
+  };
+
+
+  saveData(data);
+
+  showResult.innerHTML += `
+    <div class="Success">
+      <h3>Student ID Card! ✓</h3>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Roll Number:</strong> ${data.rollNumber}</p>
+      <p><strong>Batch Number:</strong> ${data.batch}</p>
+    </div>
+  `;
+
+  nameInput.value = "";
+  rollNoInput.value = "";
+  batchNoInput.value = "";
+}
+
+function saveData(data) {
+  let students = [];
+  let saved = localStorage.getItem("students");
+  if (saved) {
+    students = JSON.parse(saved);
+  }
+  students.push(data);
+  localStorage.setItem("students", JSON.stringify(students));
+}
